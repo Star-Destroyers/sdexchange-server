9dd7e6b7ff8a8588ea586a80c5c97e2b813ad1ef
Austin Riba <austin@m51.io>
2021-03-12 10:27:11 -0800

    CORS config and procfile

--- app/main.py
+++ app/main.py
@@ -1,10 +1,19 @@
 from fastapi import FastAPI
 
 from app.explorer.views import router as explorer_router
+from fastapi.middleware.cors import CORSMiddleware
+
 from app.config import settings
 
 app = FastAPI(title=settings.APP_NAME)
 app.include_router(explorer_router)
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=['*'],
+    allow_credentials=True,
+    allow_methods=['*'],
+    allow_headers=['*']
+)
 
 @app.get('/ping')
 async def ping():
