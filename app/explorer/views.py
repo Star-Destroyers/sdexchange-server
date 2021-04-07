f859ca26582ad78c9445dca78be7c6cb5091b4af
Austin Riba <austin@m51.io>
2021-04-07 13:46:34 -0700

    Add a few TNS endpoints

--- app/explorer/views.py
+++ app/explorer/views.py
@@ -1,6 +1,9 @@
 from fastapi import APIRouter
+from typing import List
+
 from sd_alert_pipe.lasair import LasairService
 from sd_alert_pipe.common import gather_data
+from sd_alert_pipe.tns import TNSService
 
 router = APIRouter(
     prefix='/explorer',
@@ -18,3 +21,14 @@ async def ztfobject(object_id: str) -> dict:
     result = await gather_data(object_id)
     return {**result.dict()}
 
+@router.get('/tns/cone/')
+async def tnscode(ra: float, dec: float) -> List[dict]:
+    tns = TNSService()
+    result = await tns.cone_search(ra, dec)
+    return result
+
+@router.get('/tns/detail/{objname}/')
+async def tnsdetail(objname: str) -> dict:
+    tns = TNSService()
+    result = await tns.get_tns_object(objname)
+    return result
